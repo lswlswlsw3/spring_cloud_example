ad38bddf168c1b37bc61565235ca7375dc28a2f8
Woongs <Woongs@DESKTOP-VV8CRCN>
2019-06-05 16:16:08 +0900

    Hystrix time out 동작 테스트 (step-3)

--- spring-cloud-example/work/src/main/java/com/sicc/api/WorkController.java
+++ spring-cloud-example/work/src/main/java/com/sicc/api/WorkController.java
@@ -3,6 +3,7 @@ package com.sicc.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,9 +19,15 @@ public class WorkController {
 	 * @param workId : 워크아이디
 	 * @return 문자열(워크아이디, 현시간)
 	 */
-	@GetMapping(path = "/{workId}") // path 설정 (http://localhost:8082/works/xxxx 로써 접근 가능)
+	@RequestMapping(path = "/{workId}", method = RequestMethod.GET) // path 설정 (http://localhost:8082/works/xxxx 로써 접근 가능)
 	public String getWorkDetail(@PathVariable String workId) {
-		// return String.format("[work id = %s at %s]", workId, System.currentTimeMillis());
-		throw new RuntimeException("I/O ERROR"); // fallback 테스트를 위한 코드
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "[work id = "+workId+" at "+System.currentTimeMillis()+"]";
+		//return String.format("[work id = %s at %s]", workId, System.currentTimeMillis());
+		//throw new RuntimeException("I/O ERROR"); // fallback 테스트를 위한 코드
 	}
 }
