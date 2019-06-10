4b8764b226ea74d8afe4d9b016a143650730cbb9
Woongs <Woongs@DESKTOP-VV8CRCN>
2019-06-10 11:20:49 +0900

    Circuit Breaker ThreadPool 설정 (step-5)

--- spring-cloud-example/member/src/main/java/com/sicc/service/WorkRemoteServiceImpl.java
+++ spring-cloud-example/member/src/main/java/com/sicc/service/WorkRemoteServiceImpl.java
@@ -19,11 +19,16 @@ public class WorkRemoteServiceImpl implements WorkRemoteService {
 	private RestTemplate restTemplate;
 	
 	@Override
-	@HystrixCommand(fallbackMethod = "getWorkInfoFallback") // 폴벡 메소드 발동 영역
+	@HystrixCommand(commandKey = "workInfo", fallbackMethod = "getWorkInfoFallback") // 폴벡 메소드 발동 영역
 	public String getWorkInfo(String workId) {
 		return restTemplate.getForObject(URL+workId, String.class); // work프로젝트의 URL에 따라 Object데이터 가져옴
 	}
 
+	@HystrixCommand(commandKey = "workInfo2")
+	public String getWorkInfo2(String workId2) {
+		return restTemplate.getForObject(URL+workId2, String.class);
+	}	
+	
 	// 폴벡 메소드 선언
 	public String getWorkInfoFallback(String workId, Throwable t) {
 		System.out.println("t = "+t);
